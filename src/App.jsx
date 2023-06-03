078897b7395f67910725993512aa3f5fe7ba1afa
yahyabakkar <yahyabakkar022@gmail.com>
2023-06-03 03:27:10 +0300

    NAV

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "../src/components/navbar.jsx";
 import Home from "./pages/Home.jsx";
 import Talent from "./pages/talent.jsx";
 import Profile from "./pages/Profile.jsx";
@@ -10,7 +9,6 @@ import Dashboard from "./pages/dashboard.jsx";
 export default function App() {
   return (
     <>
-      <Navbar />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
