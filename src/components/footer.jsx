95aab1cd185386bf090df3495a49204f8c176e5b
yahyabakkar <yahyabakkar022@gmail.com>
2023-06-06 03:19:49 +0300

    css

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import "./footer.css";
+
 export default function Footer() {
-  return <footer>
-    <div className="p">
-      <p>Visit Our Website:</p>
-      <a href="https://codi.tech/">Codi Tech</a>
-    </div>
-  </footer>;
+  return (
+    <footer>
+      <div className="footer-content">
+        <p>Visit Our Website:</p>
+        <a href="https://codi.tech/">Codi Tech</a>
+      </div>
+      <div className="social-media">
+        <p>Follow us on:</p>
+        <ul>
+          <li>
+            <a href="https://www.facebook.com/coditechlb">
+              <FaFacebook />
+            </a>
+          </li>
+
+          <li>
+            <a href="https://www.instagram.com/codi_tech/">
+              <FaInstagram />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
